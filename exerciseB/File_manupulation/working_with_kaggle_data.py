f61aa20adb6164de05e693b7110b18fef1363fe1
Nindenkawe <amiritius@gmail.com>
2023-12-02 11:42:00 +0200

    working wirh PDFS and tabula

--- exerciseB/File_manupulation/working_with_kaggle_data.py
+++ exerciseB/File_manupulation/working_with_kaggle_data.py
@@ -1,48 +1,35 @@
-import pandas as pd
-from datetime import datetime
+import re
+from PyPDF2 import PdfReader
 
-# Function to calculate the time difference in minutes between two time strings
-def minutes_between_times(start_time, end_time):
-    start_datetime = datetime.strptime(start_time, "%I:%M:%S %p")
-    end_datetime = datetime.strptime(end_time, "%I:%M:%S %p")
+def extract_minutes_from_pdf(pdf_file_path):
+    total_minutes = 0
 
-    # Calculate the time difference in minutes
-    time_difference = (end_datetime - start_datetime).total_seconds() / 60
+    try:
+        with open(pdf_file_path, 'rb') as pdf_file:
+            pdf_reader = PdfReader(pdf_file)
 
-    # Ensure the result is non-negative
-    return max(0, int(time_difference))
+            for page_num in range(len(pdf_reader.pages)):
+                text = pdf_reader.pages[page_num].extract_text()
 
-# Read the CSV file into a Pandas DataFrame
-df = pd.read_csv('')
+                # Use regular expression to find and extract minutes
+                minutes_matches = re.findall(r'\b(\d+)\s*minutes?\b', text, re.IGNORECASE)
 
-# Apply the minutes_between_times function to each row and add the result as a new column
-df['duration_minutes'] = df.apply(lambda row: minutes_between_times(row['start_time'], row['end_time']), axis=1)
+                # Sum the extracted minutes
+                for match in minutes_matches:
+                    total_minutes += int(match)
 
-# Calculate the total time worked in minutes
-total_minutes_worked = df['duration_minutes'].sum()
+        return total_minutes
 
-# Define the rates
-normal_rate = 80  # RWF per minute
-promotion_rate = 100  # RWF per minute during promotion
+    except FileNotFoundError:
+        print(f"Error: File '{pdf_file_path}' not found.")
+        return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
 
-# Calculate earnings at normal rate and promotion rate
-earnings_normal_rate = total_minutes_worked * normal_rate
-earnings_promotion_rate = total_minutes_worked * promotion_rate
+# Replace 'your_pdf_file.pdf' with the path to your PDF file
+pdf_file_path = 'exerciseB/File_manupulation/minutes_hist115.pdf'
+total_minutes = extract_minutes_from_pdf(pdf_file_path)
 
-# Create a bar chart to represent earnings
-rates = ['Normal Rate', 'Promotion Rate']
-earnings = [earnings_normal_rate, earnings_promotion_rate]
-
-# Plot the bar chart
-import matplotlib.pyplot as plt
-plt.figure(figsize=(8, 6))
-plt.bar(rates, earnings, color=['blue', 'green'])
-plt.ylabel('Earnings (RWF)')
-plt.title('Earnings at Different Rates')
-plt.show()
-
-# Print the total time worked and earnings per rate
-print(f"Total time worked: {total_minutes_worked} minutes")
-print(f"Earnings at normal rate ({normal_rate} RWF/minute): {earnings_normal_rate} RWF")
-print(f"Earnings at promotion rate ({promotion_rate} RWF/minute): {earnings_promotion_rate} RWF")
-df.iloc[130]
+if total_minutes is not None:
+    print(f'Total minutes: {total_minutes}')
